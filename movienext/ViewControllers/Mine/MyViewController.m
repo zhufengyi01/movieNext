c0187a190ac2db0006933e21f91179e7f07415a6
maxwelldu <dcj3sjt@163.com>
2015-03-15 18:18:06 +0800

    修复bug: 点击消息页面的用户头像进去之后无法显示用户信息

--- movienext/ViewControllers/Mine/MyViewController.m
+++ movienext/ViewControllers/Mine/MyViewController.m
@@ -70,11 +70,12 @@
     if (self.author_id&&![self.author_id isEqualToString:@"0"]) {
         //如果有用户id 并且用户的id 不为0
         [self requestUserInfo];
-    }
-    [self createTableView];
-    [self createLoadview];
+    } else {
+        [self createTableView];
+        [self createLoadview];
         [self requestData];
-    [self createToolBar];
+        [self createToolBar];
+    }
 }
 
 -(void)initData {
@@ -280,6 +281,10 @@
             }
             _userInfoDict =[responseObject objectForKey:@"detail"];
             
+            [self createTableView];
+            [self createLoadview];
+            [self requestData];
+            [self createToolBar];
         }
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         NSLog(@"Error: %@", error);
@@ -288,12 +293,8 @@
 
 }
 - (void)requestData{
-<<<<<<< HEAD
     UserDataCenter  *userCenter=[UserDataCenter shareInstance];
     if ( !_author_id |[self.author_id isEqualToString:@"0"]) {  //表示直接进入这个页面的话，这个为空
-=======
-    if ( !_author_id ) {
->>>>>>> 2591d62392d62997be880af56c3a1d810dff2490
         _author_id = userCenter.user_id;
     }
     //user_id是当前用户的ID
