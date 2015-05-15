05d9a2993eb5a25d444af5824b17586a51417603
zhufengyi01 <2323623934@qq.com>
2015-05-15 12:31:42 +0800

    切换至开发版本 ：

--- movienext/Core/Constant/Constant.h
+++ movienext/Core/Constant/Constant.h
@@ -49,7 +49,7 @@
 #define weiChatShareSecret @"ce1e16a6fa8b594e614eda18903cd22c"
 
 //#define   Review   1          ///上传appstore版本  1 为审核
-#define   Version  @"1.0.1"       //版本信息  1.0.1 审核版         1.0 正常版
+#define   Version  @"1.0"       //版本信息  1.0.1 审核版         1.0 正常版
 
 #pragma  mark 颜色设置
 #define kAppTintColor [UIColor colorWithRed:0.0/255.0 green:146.0/255.0 blue:255.0/255.0 alpha:1]
